b60f785816fb7819808381bacb6ed42a3a3742cd
dinhhuutien <dhtien0312@gmail.com>
2023-12-29 20:31:16 +0700

    update 2nd

--- SudokuDSA/src/Run/StartApp.java
+++ SudokuDSA/src/Run/StartApp.java
@@ -8,6 +8,13 @@ package Run;
  *
  * @author ADMIN
  */
+import Controller.Controller;
+import Controller.SudokuController;
+import Models.SudokuModel;
+
 public class StartApp {
-    
+	public static void main(String[] args) {
+		SudokuModel model =new SudokuModel();
+		Controller controller = new SudokuController(model);
+	}
 }
