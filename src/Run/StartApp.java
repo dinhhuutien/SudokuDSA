09b07396af9aaec3ea69c46c9c2b402398fa4ac2
dinhhuutien <100474014+dinhhuutien@users.noreply.github.com>
2024-01-02 21:07:08 +0700

    Update StartApp.java

--- src/Run/StartApp.java
+++ src/Run/StartApp.java
@@ -2,6 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/* Name: Group 19
+ Member names 
+Đinh Hữu Tiến	 ITCSIU21111
+Nguyễn Quốc Hưng ITITIU21211
+Ông Chí Viễn	 ITITIU21344
+
+*/
 package Run;
 
 /**
